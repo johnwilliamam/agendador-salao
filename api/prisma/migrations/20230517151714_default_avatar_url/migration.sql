b2dc916fc4a22067a37c74b792552728771cb6eb
John William <j,williamd.a.m@outlook.com>
2023-06-11 13:12:11 -0300

    feat: first commit

---
+++ api/prisma/migrations/20230517151714_default_avatar_url/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Schedule" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "date" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
